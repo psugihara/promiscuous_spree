d146a4ed0891ad85616ce08fa588906c5b870ef5
Peter Sugihara <peter@campsh.com>
2013-02-25 03:07:52 -0500

    added product attributes

--- admin/app/models/product_decorator.rb
+++ admin/app/models/product_decorator.rb
@@ -1,4 +1,8 @@
 Spree::Product.class_eval do
   include Promiscuous::Publisher
-  publish :name, :email
+  publish :name, :description, :available_on, :permalink, :meta_description,
+  	    	:meta_keywords, :price, :sku, :deleted_at, :prototype_id,
+  		    :option_values_hash, :on_demand, :on_hand, :weight, :height, :width, :depth,
+  		    :shipping_category_id, :tax_category_id, :product_properties_attributes,
+  		    :variants_attributes, :taxon_ids, :option_type_ids, :cost_currency
 end
